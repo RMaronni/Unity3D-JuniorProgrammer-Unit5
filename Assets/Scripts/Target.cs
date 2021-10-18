7252588e300b2383c11c26201e5c9f544294c61e
Renan Maronni <renanmaronni@gmail.com>
2021-10-18 18:33:13 +0200

    Assign a point value to each target.

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -13,6 +13,8 @@ public class Target : MonoBehaviour
 
     private GameManager gameManager; 
 
+    public int pointValue;
+
 
     // Start is called before the first frame update
     void Start()
@@ -50,7 +52,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
